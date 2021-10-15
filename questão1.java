b41e4e9b33df43e7626921224c7631ec91af2973
felipheribeiro <89546698+felipheribeiro@users.noreply.github.com>
2021-10-15 20:37:07 -0300

    Create questão1.java

---
+++ questão1.java
@@ -0,0 +1,16 @@
+public class Paciente {
+
+   int String codigo, nome, dataNascimento, sexo, planoSaude, alergia, tipoSanguineo;	
+ 
+   public Paciente(int codigo, String nome, String dataNasc, String sexo, String planoSaude, String alergia, String tipoSang){
+
+        this.codigo = codigo;
+        this.nome = nome;
+        this.dataNascimento = dataNasc;
+        this.sexo = sexo;
+        this.planoSaude = planoSaude;
+        this.alergia = alergia;
+        this.tipoSanguineo = tipoSang;
+		
+	 }
+}

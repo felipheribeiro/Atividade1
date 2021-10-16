18f17969d6b9b5f04447869810632ec0c9acb449
felipheribeiro <89546698+felipheribeiro@users.noreply.github.com>
2021-10-15 22:03:33 -0300

    Create questão5.java

---
+++ questão5.java
@@ -0,0 +1,20 @@
+public class Aluno {
+
+        double Media;
+        Aluno nmr1 = new Q5_aluno("Luiz Feliphe Silva Ribeiro", "105.967.015-19");
+
+        nmr1.nota1 = 10.0;
+        nmr1.nota2 = 7.0;
+        nmr1.nota3 = 3.0;
+
+        Media = nmr.calcularMedia();
+
+        System.out.printf("\nMedia: %.2f", media);
+
+        nmr1.nota2 = 6.0;
+
+       media = nmr1.calcularMedia();
+
+        System.out.printf("\nMedia: %.2f", media);
+    }
+}

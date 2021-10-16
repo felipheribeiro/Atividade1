2b133a478c9f053d57d32b3fd98fcea7dc4a569c
felipheribeiro <89546698+felipheribeiro@users.noreply.github.com>
2021-10-15 21:19:39 -0300

    Create questão2_medico.java

---
+++ questão2_medico.java
@@ -0,0 +1,14 @@
+public class Q2_medico {
+
+    int codigo; String nome, sexo, especialidade; endereco;
+
+    public Q2_medico(int cod, String nome, String sexo, String especialidade, String endereco){
+
+        this.codigo = cod;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.especialidade = especialidade;
+        this.endereco = endereco;
+        
+    }
+}

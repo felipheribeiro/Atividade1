da320a98e1675f8d7357290452ad078ff30ba605
felipheribeiro <89546698+felipheribeiro@users.noreply.github.com>
2021-10-15 21:14:54 -0300

    Create questão2_endereco.java

---
+++ questão2_endereco.java
@@ -0,0 +1,15 @@
+public class Endereco {
+
+    String rua, complemento, bairro, cidade, uf, numero, cep;
+
+    public Endereco(String rua, String numero, String complemento, String bairro, String cidade, String uf, String cep){
+
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cep = cep;
+    }
+}
